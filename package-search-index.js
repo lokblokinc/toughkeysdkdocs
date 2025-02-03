e278bb2d61338b4bc5d88cff3a38a127da40124f
Michael Perretta <mike.perretta@lokblok.co>
2025-02-03 07:24:20 -0800

    Toughkeylib docs updated to version 0.2

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"co.lokblok.toughkey_android_library"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"co.lokblok.toughkeylib"}];updateSearchResults();
